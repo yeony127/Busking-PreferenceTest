8dc855ac4989b0c02c90162606ade451a6cc630b
yeony127 <yeony127@hanmail.net>
2026-09-06 01:43:55 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,16 +2,19 @@
 // - Chrome이 "홈 화면에 추가"를 진짜 설치형 PWA(주소창 없음)로 인식하게 하는 데 필요합니다.
 // - 부가 효과로 행사장 와이파이가 불안정해도 이미 방문한 화면은 캐시에서 빠르게 열립니다.
 
-const CACHE_NAME = "gimpo-busking-quiz-v3";
+const CACHE_NAME = "gimpo-busking-quiz-v5";
 const CORE_ASSETS = [
   "./",
   "./index.html",
   "./manifest.json",
   "./icons/icon-192.png",
   "./icons/icon-512.png",
+  "./icons/apple-touch-icon.png",
+  "./icons/favicon-32.png",
   "./fonts/noto-sans-kr-subset-400.woff2",
   "./fonts/noto-sans-kr-subset-700.woff2",
-  "./fonts/jua-subset-400.woff2"
+  "./fonts/jua-subset-400.woff2",
+  "./assets/banner.jpg"
 ];
 
 self.addEventListener("install", (event) => {
